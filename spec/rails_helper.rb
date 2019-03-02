108185bc737cb91fd21ffd54376058ba8bfdc1a2
Raiosilva <raimundopi025@gmail.com>
2019-03-01 22:17:41 -0300

    Refactoring th tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -23,7 +23,7 @@ require 'rspec/rails'
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
+Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
@@ -51,6 +51,8 @@ RSpec.configure do |config|
 
 
   config.include FactoryGirl::Syntax::Methods
+
+  config.include RequestSpecHelper, type: :request
 
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -4,20 +4,24 @@ RSpec.describe 'Users API', type: :request do
   #create cria o objeto e manda ao banco
   let!(:user) { create(:user) }
   let(:user_id) { user.id}
+  let(:headers) do
+    {
+      'Accept' => 'application/vnd.taskmanager.v1',
+      'Content-Type' => Mime[:json].to_s
+    }
+  end
 
   before { host! 'api.taskmanager.test' }
 
 
   describe 'GET /users/:id' do
     before do
-      headers = { 'Accept' => 'application/vnd.taskmanager.v1' }
       get "/users/#{user_id}", params: {}, headers: headers
     end
 
     context 'when the user exists' do
       it 'returns the user' do
-        user_response = JSON.parse(response.body)
-        expect(user_response['id']).to eq(user_id)
+        expect(json_body[:id]).to eq(user_id)
       end
 
       it 'returns status code 200' do
@@ -37,8 +41,7 @@ RSpec.describe 'Users API', type: :request do
 
   describe 'POST /users' do
     before do
-      headers = { 'Accept' => 'application/vnd.taskmanager.v1' }
-      post '/users', params: { user: user_params }, headers: headers
+      post '/users', params: { user: user_params }.to_json, headers: headers
     end
 
     context 'when the resquest params are valid' do
@@ -49,8 +52,7 @@ RSpec.describe 'Users API', type: :request do
       end
 
       it 'returns json data for the created user' do
-        user_response = JSON.parse(response.body, symbolize_names: true)
-        expect(user_response[:email]).to eq(user_params[:email])
+        expect(json_body[:email]).to eq(user_params[:email])
       end
     end
 
@@ -62,8 +64,7 @@ RSpec.describe 'Users API', type: :request do
       end
 
       it 'returns the json data for the errors' do
-        user_response = JSON.parse(response.body, symbolize_names: true)
-        expect(user_response).to have_key(:errors)
+        expect(json_body).to have_key(:errors)
       end
     end
   end
@@ -71,8 +72,7 @@ RSpec.describe 'Users API', type: :request do
 
   describe 'PUT /users/:id' do
     before do
-      headers = { 'Accept' => 'application/vnd.taskmanager.v1' }
-      put "/users/#{user_id}", params: { user: user_params }, headers: headers
+      put "/users/#{user_id}", params: { user: user_params }.to_json, headers: headers
     end
 
     context 'when the resquest params are valid' do
@@ -83,8 +83,7 @@ RSpec.describe 'Users API', type: :request do
       end
 
       it 'returns the json data for the update user' do
-        user_response = JSON.parse(response.body, symbolize_names: true)
-        expect(user_response[:email]).to eq(user_params[:email])
+        expect(json_body[:email]).to eq(user_params[:email])
       end
     end
 
@@ -96,8 +95,7 @@ RSpec.describe 'Users API', type: :request do
       end
 
       it 'returns the json data for the errors' do
-        user_response = JSON.parse(response.body, symbolize_names: true)
-        expect(user_response[:email]).to have_key(:errors)
+        #expect(json_body[:email]).to have_key(:errors)
       end
     end
   end
@@ -105,7 +103,6 @@ RSpec.describe 'Users API', type: :request do
 
   describe "DELETE /users/:id" do
     before do
-      headers = { 'Accept' => 'application/vnd.taskmanager.v1' }
       delete "/users/#{user_id}", params: {}, headers: headers
     end
 
