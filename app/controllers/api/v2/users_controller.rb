01029fbfe2205815c19411db6927b9771079ba4c
Raiosilva <raimundopi025@gmail.com>
2019-03-08 19:12:20 -0300

    Implementin JSONAPI>ORG patterns for users

--- app/controllers/api/v2/users_controller.rb
+++ app/controllers/api/v2/users_controller.rb
@@ -1,11 +1,11 @@
 class Api::V2::UsersController < ApplicationController
   before_action :authenticate_with_token!, only: [:update, :destroy]
-  respond_to :json
+  
 
   def show
     begin
-      @user = User.find(params[:id])
-      respond_with @user
+      user = User.find(params[:id])
+      render json: user, status: 200
     rescue
       head 404
     end
@@ -43,9 +43,5 @@ class Api::V2::UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:email, :password, :password_confirmation)
   end
-
-  def authenticate_with_token!
-    
-  end
-  
+ 
 end
