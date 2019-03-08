9d13b71d4b06a1f3a2644aece6a45dd6a0a80bb5
Raiosilva <raimundopi025@gmail.com>
2019-03-08 10:42:15 -0300

    adding association with tasks for model user and task

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe User, type: :model do
   #builld test de memorio cria o objeto e mantem em memoria.
   let(:user) { build(:user) }
 
+  it { is_expected.to have_many(:tasks).dependent(:destroy) }
+
   it { is_expected.to validate_presence_of(:email) }
   it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
   it { is_expected.to validate_confirmation_of(:password) }
@@ -62,7 +64,4 @@ RSpec.describe User, type: :model do
   #end
 
   #it { is_expected.to validate_presence_of(:name) }
-
-
-
 end
